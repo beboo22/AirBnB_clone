bfb043ae36397e45f37ee70cc217f3b500b43c67
beboo22 <mt557829@gmail.com>
2023-08-12 16:22:46 +0300

    task

--- models/base_model.py
+++ models/base_model.py
@@ -19,14 +19,18 @@ class BaseModel:
         instatiates an object with it's
         attributes
         """
-        DATE_TIME_FORMAT = '%Y-%m-%dT%H:%M:%S.%f'
-        if not kwargs:
-            self.id = str(uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-        else:
+        if len(kwargs) > 0:
             for key, value in kwargs.items():
-                self.__dict__[key] = value.isoformat()
+                if key == '__class__':
+                    continue
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.fromisoformat(value)
+                setattr(self, key, value)
+            return
+
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
         """
